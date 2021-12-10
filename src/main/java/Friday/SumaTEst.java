66761373e4f00549c167bc6e4cc5bb7eec32db4f
kranjan <kranjan@RanjanKumar>
2021-12-10 13:20:46 +0530

    done3

--- src/main/java/Friday/SumaTEst.java
+++ src/main/java/Friday/SumaTEst.java
@@ -8,11 +8,11 @@ public class SumaTEst {
 		System.out.println("Friday practice");
 
 		System.out.println("rrrrrrr33333");
-		System.out.println("dooooooo1111");
+		System.out.println("doohhhhhooooo1111");
 
 		System.out.println("done");
 		System.out.println("done2");
-		System.out.println("done");
+		System.out.println("done3");
 
 	}
 
